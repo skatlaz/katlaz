2186da282d7861de8f61f9861e9091619cadbf15
skatlaz <webmaster@skatlaz.com>
2026-03-28 02:24:23 -0300

    Update build.bat

--- src/build.bat
+++ src/build.bat
@@ -6,12 +6,12 @@ cd /d %~dp0
 SET MSYS2_PATH=C:\msys64
 
 IF NOT EXIST "%MSYS2_PATH%\usr\bin\bash.exe" (
-    echo MSYS2 nao encontrado!
+    echo MSYS2 not found!
     pause
     exit /b
 )
 
-echo Compilando...
+echo Compiling...
 
 "%MSYS2_PATH%\usr\bin\bash.exe" -lc "cd $(cygpath '%cd%') && g++ app/AppMain.cpp -o my_app.exe `pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0`"
 
