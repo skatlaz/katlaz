84567fddfe868c1d4c5e7a0a65232f502d48ac8f
skatlaz <webmaster@skatlaz.com>
2026-03-28 22:31:06 -0300

    Add files via upload

--- katlazcc/__init__.py
+++ katlazcc/__init__.py
@@ -1,3 +1,10 @@
 # __init__.py
-from .version import __version__
-from .katlazcc import *
+import sys
+from version import __version__
+from katlazcc.analyzer import *
+from katlazcc.builder import *
+from katlazcc.c_generator import *
+from katlazcc.cli import *
+from katlazcc.lexer import *
+from katlazcc.parser import *
+from katlazcc.transpiler import *
