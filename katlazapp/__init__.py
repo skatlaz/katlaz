ab063cd31472c264db17c33761aeea9fa41b94a4
skatlaz <webmaster@skatlaz.com>
2026-03-30 01:27:13 -0300

    Delete katlazapp/__init__.py

--- katlazapp/__init__.py
+++
@@ -1,6 +0,0 @@
-from katlazapp.ast.nodes import *
-from katlazapp.compiler.parser import *
-from katlazapp.compiler.transpiler import *
-from katlazapp.runtime.bridge import *
-from katlazapp.runtime.core import *
-from katlazapp.runtime.loader import *
